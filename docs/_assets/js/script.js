3a5871ec58ae8d4bbac59551281f760a58293659
SusieHatter <susiivaldez@gmail.com>
2021-06-27 21:54:57 -0500

    Replaced custom CSS with bootstrap 5.0

--- docs/_assets/js/script.js
+++ docs/_assets/js/script.js
@@ -1,6 +1,1 @@
 // THIS FILE IS GENERATED. DO NOT ALTER. EDIT THE CONTENTS IN src\site\_assets\js\script.js
-const mainNav = document.getElementById("js-menu");
-const navBarToggle = document.getElementById("js-navbar-toggle");
-navBarToggle.addEventListener("click", function () {
-  mainNav.classList.toggle("active");
-});
--- src/site/_assets/js/script.js
+++
@@ -1,5 +0,0 @@
-const mainNav = document.getElementById("js-menu");
-const navBarToggle = document.getElementById("js-navbar-toggle");
-navBarToggle.addEventListener("click", function () {
-  mainNav.classList.toggle("active");
-});
