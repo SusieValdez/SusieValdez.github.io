3def04419cfde1907d51e1b55fa8850d9776adbc
SusieHatter <susiivaldez@gmail.com>
2021-06-28 20:58:59 -0500

    Add recently read cards

--- src/site/recently-read/page.js
+++ src/site/recently-read/page.js
@@ -1,7 +1,24 @@
-const bardItem = (obj) =>
-  `<div class='bard-item'>
-    <a href="${obj.link}">${obj.title}</a>
+const TITLE_CUTOFF = 50;
+
+const bardItem = (obj) => {
+  const description = "description";
+  const title =
+    obj.title.length > TITLE_CUTOFF
+      ? obj.title.slice(0, TITLE_CUTOFF) + "..."
+      : obj.title;
+  return `<div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">
+          ${description}
+        </p>
+        <a href="${obj.link}" class="btn btn-primary">Read it here</a>
+      </div>
+    </div>
   </div>`;
+};
+
 const recentlyReadItemsEl = document.getElementById("bard-items");
 fetch("https://raw.githubusercontent.com/SusieHatter/bard/main/items.json")
   .then((res) => res.json())
