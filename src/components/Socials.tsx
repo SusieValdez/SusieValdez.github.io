388d495a86904cc177bbcf1e0c51f07321eb5988
SusieHatter <susiivaldez@gmail.com>
2022-05-06 10:33:05 -0500

    Add links that open in new tabs

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -5,24 +5,31 @@ import linkedinSolid from "../images/linkedin-brands.svg";
 
 const Socials = () => {
   return (
-    <div className="flex flex-col bg-transparent text-base text-violet-200">
+    <div className="hidden md:flex flex-col bg-transparent text-base text-violet-200 ">
       <button className="my-3 hover:animate-pulse hover:text-pink-500 hover:-translate-y-1 transition duration-150 ease-out  hover:ease-in">
-        <img
-          style={{
-            filter:
-              "invert(86%) sepia(28%) saturate(901%) hue-rotate(191deg) brightness(102%) contrast(102%)",
-          }}
-          src={githubSolid}
-        />
+        <a href="https://github.com/SusieValdez" target="_blank">
+          <img
+            style={{
+              filter:
+                "invert(86%) sepia(28%) saturate(901%) hue-rotate(191deg) brightness(102%) contrast(102%)",
+            }}
+            src={githubSolid}
+          />
+        </a>
       </button>
       <button className="my-3 hover:animate-pulse hover:text-pink-500 hover:-translate-y-1 transition duration-150 ease-out  hover:ease-in">
-        <img
-          style={{
-            filter:
-              "invert(86%) sepia(28%) saturate(901%) hue-rotate(191deg) brightness(102%) contrast(102%)",
-          }}
-          src={linkedinSolid}
-        />
+        <a
+          href="https://www.linkedin.com/in/susievaldezmireles/"
+          target="_blank"
+        >
+          <img
+            style={{
+              filter:
+                "invert(86%) sepia(28%) saturate(901%) hue-rotate(191deg) brightness(102%) contrast(102%)",
+            }}
+            src={linkedinSolid}
+          />
+        </a>
       </button>
       <button className="w-5 h-5 my-3 hover:animate-pulse hover:text-pink-500 hover:-translate-y-1 transition duration-150 ease-out  hover:ease-in">
         <img
